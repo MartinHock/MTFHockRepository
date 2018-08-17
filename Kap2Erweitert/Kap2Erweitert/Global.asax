5fcc4ad6a0938b7631ad2916e10fadaa2495a0a1
Martin <martin.hock@icloud.com>
2018-08-17 14:57:48 +0200

    added kap2 erweitert

---
+++ Kap2Erweitert/Kap2Erweitert/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Kap2Erweitert.MvcApplication" Language="C#" %>
---
+++ Kap2Erweitert/Kap2Erweitert/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Kap2Erweitert.Startup))]
+namespace Kap2Erweitert
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
