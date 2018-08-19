b07cffa62a4d4dddc66e3072acd1b834f5d504c9
Martin <martin.hock@icloud.com>
2018-08-19 09:59:19 +0200

    aaded thuungs frm tutorial

--- Kap2Erweitert/Kap2Erweitert/Controllers/HomeController.cs
+++ Kap2Erweitert/Kap2Erweitert/Controllers/HomeController.cs
@@ -3,28 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Kap2Erweitert.Models;
+using Kap2Erweitert.Infrastructure;
+
+    Stephan, Uwe.Grundlagen ASP.NET MVC 5: An Beispielen mit C#, HTML5, und CSS erklärt (German Edition) (Kindle-Positionen634-640). Kindle-Version. 
 
 namespace Kap2Erweitert.Controllers
 {
-    public class HomeController : Controller
-    {
-        public ActionResult Index()
-        {
-            return View();
-        }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+    [SessionState( System .Web .SessionState .SessionStateBehavior .Default)]
+    // GET: Home 
+    public ActionResult Index() { Info info = new Info { ControllerName = "Home", ActionName = "Index", Aufrufe = "nicht gefunden" }; // wenn erster Aufruf, dann Initialisieren if(Session != null && Session["Aufrufe"] == null) { Session["Aufrufe"] = 0; } // Aufrufanzahl in Session erhöhen if(Session != null && Session["Aufrufe"] != null) { Session[ "Aufrufe"] = (int)Session[ "Aufrufe"] + 1;
 
-            return View();
-        }
+     {
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
 
-            return View();
-        }
+
     }
 }
---
+++ Kap2Erweitert/Kap2Erweitert/Models/Info.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kap2Erweitert.Models
+{
+    public class Info
+    {
+        public string ControllerName { get; set; }
+        public string ActionName { get; set; }
+        public string Aufrufe { get; set; }
+
+    }
+}
+
